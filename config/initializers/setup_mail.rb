a77d24d62967b1dc1c0caae5c24036da4cfbc2ee
DEV MODE <devmode@Jays-MacBook-Pro.local>
2015-01-25 11:05:38 -0800

    testing with deployment to heroku

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,15 +1,15 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :enable_starttls_auto => true,
-  # :domain             => 'localhost',
-  :user_name            => 'jaycdave@gmail.com',
-  :password             => 'zumiez6431',
-  :authentication       => "login",
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.perform_deliveries = true
+# ActionMailer::Base.raise_delivery_errors = true
+# ActionMailer::Base.smtp_settings = {
+#   :address              => "smtp.gmail.com",
+#   :port                 => 587,
+#   :enable_starttls_auto => true,
+#   # :domain             => 'localhost',
+#   :user_name            => 'jaycdave@gmail.com',
+#   :password             => 'zumiez6431',
+#   :authentication       => "login",
+#   :enable_starttls_auto => true
+# }
 
-ActionMailer::Base.default_url_options = { :host => 'localhost', :port => 3000 }
+# ActionMailer::Base.default_url_options = { :host => 'localhost', :port => 3000 }
