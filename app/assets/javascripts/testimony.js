a5083163eed056730ea8801bb66e22bb34006423
Jay Dave <jaycdave@gmail.com>
2015-02-08 12:49:56 -0800

    fixed testimony

--- app/assets/javascripts/testimony.js
+++ app/assets/javascripts/testimony.js
@@ -1,13 +1,10 @@
 $("#owl-text").owlCarousel({
     autoPlay : 6000,
     stopOnHover : true,
-    navigation:true,
-    paginationSpeed : 1400,
-    goToFirstSpeed : 100,
-    singleItem : true,
-    autoHeight : true,
-    transitionStyle:"fade",
-    pagination:false
+     navigation:true,
+     singleItem : true,
+     autoHeight : true,
+     pagination:false
 });
 
 
