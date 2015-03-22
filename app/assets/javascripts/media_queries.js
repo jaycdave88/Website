5ec61475d1ed330a79cf0e1371c2681714d0680b
Jay Dave <jaycdave@gmail.com>
2015-03-21 23:36:23 -0700

    changed layout and buttons and also added media view port for screen size less thatn 700 px

--- app/assets/javascripts/media_queries.js
+++ app/assets/javascripts/media_queries.js
@@ -1,1 +1,11 @@
-// create media queries for diffrent screen sizes to 
+var mql = window.matchMedia("screen and (min-width: 800px)")
+if (mql.matches){ 
+	$('#expander').hide();
+	$('.content').show();
+}
+else{
+	$('#expander').simpleexpand();  
+}
+
+
+
