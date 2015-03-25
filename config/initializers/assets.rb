af6cf9662c920b151b1402f2a54b20fc96365f5d
Jay Dave <jaycdave@gmail.com>
2015-03-25 05:10:28 -0700

    fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,10 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( loadScreen.js )
 Rails.application.config.assets.precompile += %w( stylez.css )
+Rails.application.config.assets.precompile += %w( up.png )
+
 
 
 # Precompile additional assets.
