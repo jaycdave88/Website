0d75e48b615fc18b04d519887271c19ce3dd2464
Jay Dave <jaycdave@gmail.com>
2015-05-04 16:26:05 -0700

    fixed the smooth scroll icon and fixed layout issues with sidebar -removed bootstrap cdn replaced with font awesome - working code

--- app/assets/javascripts/topButton.js
+++ app/assets/javascripts/topButton.js
@@ -1,14 +1,24 @@
-$(window).scroll(function(){
-		if ($(this).scrollTop() > 500) {
-			$('.scrollToTop').fadeIn();
-		} else {
-			$('.scrollToTop').fadeOut();
-		}
-	});
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 500) {
+        $('.scrollToTop').fadeIn();
+    } else {
+        $('.scrollToTop').fadeOut();
+    }
 
-	//Click event to scroll to top
-	$("#top").click(function(){
-		$('html, body').stop().animate({scrollTop : 0},500);
-		return false;
-	});
+    $("#top").click(function() {
+        if ($(this).scrollTop() > 500) {
 
+            $("html, body").stop().animate({
+                scrollTop: 0
+            }, "slow");
+        }
+    });
+});
+
+$("#top").click(function() {
+
+    $('html, body').stop().animate({
+        scrollTop: 0
+    }, "slow");
+    return false;
+});
