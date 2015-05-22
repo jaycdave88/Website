2beb0d361083a28f163461cc45efd073747c7797
Jay Dave <jaycdave@gmail.com>
2015-05-22 02:31:41 -0700

    added grid to better UI and fixed some issues git status

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
 	def create
 		@post = Post.new(post_params)
 		if @post.save
-			redirect_to @post
+			redirect_to posts_path
 		else
 			render 'new'
 		end
