839db5695348bd49fb7f73dc6dc65b9f3fded485
Jay Dave <jaycdave@gmail.com>
2017-03-10 14:30:04 -0600

    puhing

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,3 +1,4 @@
+ActionMailer::Base.default_url_options = { :host => 'website-jaycdave.herokuapp.com' }  
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
@@ -5,7 +6,7 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :enable_starttls_auto => true,
-  :domain             => 'localhost',
+  :domain             => 'website-jaycdave.herokuapp.com',
   :user_name            => 'jaycdave@gmail.com',
   :password             => 'Zumiez6431$!',
   :authentication       => "plain"
